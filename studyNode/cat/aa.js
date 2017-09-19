abab4dd0cd280b47a94833c33442602c7a79e3fc
curry <17828455424@163.com>
2017-09-19 15:56:21 +0800

    node change

--- studyNode/cat/aa.js
+++ studyNode/cat/aa.js
@@ -4,3 +4,15 @@
 let a1 = require("./a1");
 let a2 = require("./a2");
 console.log("hell cat");
+let fs = require('fs');
+
+function copy(src, dst) {
+    fs.writeFileSync(dst, fs.readFileSync(src));
+}
+
+function main(argv) {
+    console.log(argv);
+    copy(argv[0], argv[1]);
+}
+
+main(process.argv.slice(2));
