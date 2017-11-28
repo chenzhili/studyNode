1b46779f7d474b281fc622052c6169db7aa3b88b
curry <17828455424@163.com>
2017-11-28 18:35:44 +0800

    webserver change

---
+++ studyNode/webServer/wepApp/index.js
@@ -0,0 +1,5 @@
+/**
+ * Created by YK on 2017/11/28.
+ */
+const start = require("./server");
+start.start();
--- studyNode/webServer/wepApp/server.js
+++ studyNode/webServer/wepApp/server.js
@@ -2,9 +2,17 @@
  * Created by YK on 2017/11/23.
  */
 const http = require("http");
+const url = require("url");
 
-http.createServer((request,response)=>{
-    response.writeHead(200,{"Content-Type":"text/plain"});
-    response.write("hello world");
-    response.end();
-}).listen(8989);
+function start(){
+    http.createServer((request,response)=>{
+        let pathname = url.parse(request.url).pathname;
+        console.log(pathname);
+        console.log(request.url);
+        response.writeHead(200,{"Content-Type":"text/plain"});
+        response.write("hello world");
+        response.end();
+    }).listen(8989);
+}
+/*module.exports=start;*//*这种写法，可以把文件输出进行 重写为 start*/
+exports.start = start;/*这个向外暴露的是 exports 这个对象，不只是 start 这个方法*/
