dfae700e91e6aaa869cb81cab36bc4867056cb82
chenzhili <1845882620@qq.com>
2018-05-29 15:49:56 +0800

    algorithm add

--- AlgorithmAndStructure/2018-5-28.js
+++ AlgorithmAndStructure/2018-5-28.js
@@ -32,8 +32,11 @@ let fibonacci = (n)=>{
 /**
  * 质数
  * @param {*} n 
+ * 这里需要优化，减少 循环查询
  */
 let prime = (n)=>{
+    if(n==1 || n == 2 || n==3){return true;}
+    if(n%2 == 0){return false;}
     for(let i=2;i<n;i++){
         if(n%i == 0){
             return false;
@@ -41,4 +44,69 @@ let prime = (n)=>{
     }
     return true;
 }
-console.log(prime(99090901));
+// console.log(prime(99090901));
+
+/**
+ * 欧几里得算法
+ * @param {*} a 整数
+ * @param {*} b 整数
+ */
+let GCD = (a,b)=>{
+    let min = Math.min(a,b);
+    let max = Math.max(a,b);
+
+    let mod = -1;
+    let result = -1;
+    while(mod != 0){
+        mod = max%min;
+        max = Math.max(mod,min);
+        min = Math.min(mod,min);
+    }
+    return max;
+}
+// console.log(GCD(252000,1050000));
+
+/**这是阮一峰的写法
+ * @param {number} originalA
+ * @param {number} originalB
+ * @return {number|null}
+ */
+let euclideanAlgorithm = function (originalA, originalB) {
+    const a = Math.abs(originalA);
+    const b = Math.abs(originalB);
+  
+    if (a === 0 && b === 0) {
+      return null;
+    }
+  
+    if (a === 0 && b !== 0) {
+      return b;
+    }
+  
+    if (a !== 0 && b === 0) {
+      return a;
+    }
+  
+    // Normally we need to do subtraction (a - b) but to prevent
+    // recursion occurs to often we may shorten subtraction to (a % b).
+    // Since (a % b) is normally means that we've subtracted b from a
+    // many times until the difference became less then a.
+  
+    if (a > b) {
+      return euclideanAlgorithm(a % b, b);
+    }
+  
+    return euclideanAlgorithm(b % a, a);
+  }
+//   console.log(euclideanAlgorithm(252000,1050000));
+
+/**
+ * least common multiple(最小公倍数)
+ * 两个自然数的乘积等于这两个自然数的最大公约数和最小公倍数的乘积
+ * @param {*} a 
+ * @param {*} b 
+ */
+let lcm = (a,b)=>{
+    return a*b/(GCD(a,b))
+}
+console.log(lcm(10,12));
